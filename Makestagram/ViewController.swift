30d98b331613fb32ce5be07cc9b07c2ae80bcbd2
Chase Wang <hi@ocwang.com>
2017-03-08 11:28:16 -0800

    Initial commit

---
+++ Makestagram/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Makestagram
+//
+//  Created by Chase Wang on 3/8/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# makestagram-v3
